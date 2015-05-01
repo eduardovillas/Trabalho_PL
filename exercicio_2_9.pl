f51ca557ddd25a69d347abb40c0dbbf73808ac58
lucasmartioli <lucasmartioli12@hotmail.com>
2015-05-01 14:43:53 -0300

    Corrigindo erros nos testes!

--- exercicio_2_9.pl
+++ exercicio_2_9.pl
@@ -1,10 +1,12 @@
 :- use_module(library(plunit)).
 
-:- begin_tests(soma quadrado maiores).
+:- begin_tests(soma_quadrado_maiores).
 
-	test(soma quadrado maiores1) :- soma quadrado maiores(1,2,2,8).
-	test(soma quadrado maiores2) :- soma quadrado maiores(1,1,1,2).
-	test(soma quadrado maiores1) :- soma quadrado maiores(4,1,2,20).
-	test(soma quadrado maiores1) :- \+soma quadrado maiores(3,2,4,24).	
+	test(soma_quadrado_maiores1, S == 8) :- soma_quadrado_maiores(1,2,2,S).
+	test(soma_quadrado_maiores1) :- soma_quadrado_maiores(1,2,2,8).
+	test(soma_quadrado_maiores2) :- soma_quadrado_maiores(1,1,1,2).
+	test(soma_quadrado_maiores3) :- soma_quadrado_maiores(4,1,2,20).
+	test(soma_quadrado_maiores4) :- \+soma_quadrado_maiores(3,2,4,24).	
 
-:- end_tests(soma quadrado maiores).
+:- end_tests(soma_quadrado_maiores).
+
