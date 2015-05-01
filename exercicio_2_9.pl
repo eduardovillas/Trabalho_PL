8ea9b921c6fc57e10f67793daa01efb032a6175f
lucasmartioli <lucasmartioli12@hotmail.com>
2015-05-01 15:42:30 -0300

    Melhorias

--- exercicio_2_9.pl
+++ exercicio_2_9.pl
@@ -1,3 +1,7 @@
+%% quadrado(+A, +B, +C, ?S) is semidet
+%
+%  Verdadeiro se S é a soma do maior dentre os três (A,B,C).
+
 :- use_module(library(plunit)).
 
 :- begin_tests(soma_quadrado_maiores).
@@ -18,37 +22,16 @@
 :- end_tests(menorDosTres).
 
 soma_quadrado_maiores( A, B, C, S ) :-
-var(S),
 menorDosTres(A,B,C),
-S is B*B + C*C.
+S is B*B + C*C, !.
 
 soma_quadrado_maiores( A, B, C, S ) :-
-nonvar(S),
-menorDosTres(A,B,C),
-S1 is B*B + C*C,
-S == S1.
-
-soma_quadrado_maiores( A, B, C, S ) :-
-var(S),
 menorDosTres(B,A,C),
-S is A*A + C*C.
+S is A*A + C*C, !.
 
 soma_quadrado_maiores( A, B, C, S ) :-
-nonvar(S),
-menorDosTres(B,A,C),
-S1 is A*A + C*C,
-S == S1.
-
-soma_quadrado_maiores( A, B, C, S ) :-
-var(S),
 menorDosTres(C,B,A),
-S is B*B + A*A.
-
-soma_quadrado_maiores( A, B, C, S ) :-
-nonvar(S),
-menorDosTres(C,B,A),
-S1 is B*B + A*A,
-S == S1.
+S is B*B + A*A, !.
 
 menorDosTres(MENOR, OUTRO1, OUTRO2) :-
 MENOR < OUTRO1, MENOR < OUTRO2.
