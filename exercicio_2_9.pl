0f5a30f2463e3a7b71b45d21500eb168f7b5e3bb
lucasmartioli <lucasmartioli12@hotmail.com>
2015-05-01 14:39:26 -0300

    Implementação dos testes

--- exercicio_2_9.pl
+++ exercicio_2_9.pl
@@ -1,1 +1,10 @@
- 
+:- use_module(library(plunit)).
+
+:- begin_tests(soma quadrado maiores).
+
+	test(soma quadrado maiores1) :- soma quadrado maiores(1,2,2,8).
+	test(soma quadrado maiores2) :- soma quadrado maiores(1,1,1,2).
+	test(soma quadrado maiores1) :- soma quadrado maiores(4,1,2,20).
+	test(soma quadrado maiores1) :- \+soma quadrado maiores(3,2,4,24).	
+
+:- end_tests(soma quadrado maiores).
