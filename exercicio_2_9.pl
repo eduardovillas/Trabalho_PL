bbf46d7a97777212d32b3ea5c6df1bb168e4e047
lucasmartioli <lucasmartioli12@hotmail.com>
2015-05-01 15:30:53 -0300

    Exercicio Finalizado

--- exercicio_2_9.pl
+++ exercicio_2_9.pl
@@ -10,3 +10,45 @@
 
 :- end_tests(soma_quadrado_maiores).
 
+:- begin_tests(menorDosTres).
+
+	test(menorDosTres1) :- menorDosTres(1,2,3).
+	test(menorDosTres1) :- menorDosTres(2,1,3).	
+	
+:- end_tests(menorDosTres).
+
+soma_quadrado_maiores( A, B, C, S ) :-
+var(S),
+menorDosTres(A,B,C),
+S is B*B + C*C.
+
+soma_quadrado_maiores( A, B, C, S ) :-
+nonvar(S),
+menorDosTres(A,B,C),
+S1 is B*B + C*C,
+S == S1.
+
+soma_quadrado_maiores( A, B, C, S ) :-
+var(S),
+menorDosTres(B,A,C),
+S is A*A + C*C.
+
+soma_quadrado_maiores( A, B, C, S ) :-
+nonvar(S),
+menorDosTres(B,A,C),
+S1 is A*A + C*C,
+S == S1.
+
+soma_quadrado_maiores( A, B, C, S ) :-
+var(S),
+menorDosTres(C,B,A),
+S is B*B + A*A.
+
+soma_quadrado_maiores( A, B, C, S ) :-
+nonvar(S),
+menorDosTres(C,B,A),
+S1 is B*B + A*A,
+S == S1.
+
+menorDosTres(MENOR, OUTRO1, OUTRO2) :-
+MENOR < OUTRO1, MENOR < OUTRO2.
