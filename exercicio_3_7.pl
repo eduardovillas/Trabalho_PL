f9917a87e53a3a728f8d2e378f1ea07548f2b42c
lucasmartioli <lucasmartioli12@hotmail.com>
2015-06-28 18:11:15 -0300

    Mitando

--- exercicio_3_7.pl
+++ exercicio_3_7.pl
@@ -1,25 +1,25 @@
 
-%% removido_em(+L, +X, +I, ?R) is semidet
+%% inserido_em(+L, +X, +I, ?R) is semidet
 %
 % é verdadeiro se R é a lista L com o elemento X inserido da posição I.
 
-:- begin_tests(removido_em).
+:- begin_tests(inserido_em).
 
-	test(removido_em1) :- removido_em([1,2,3], 2, 2, [1,3]).	
-	test(removido_em2) :- removido_em([1], 1, 1, []).	
-	test(removido_em3, fail) :- removido_em([1,2], 1, 2, [1]).	
-	test(removido_em4) :- removido_em([1,2,3], 1, 1, [2,3]).	
-	test(removido_em5) :- removido_em([1,2,4], 4, 3, [1,2]).
-	test(removido_em6, fail) :- removido_em([1,4], 2, 2, [1]).
+	test(inserido_em1) :- inserido_em([1,3], 2, 2, [1,2,3]).	
+	test(inserido_em2) :- inserido_em([], 1, 1, [1]).	
+	test(inserido_em3, fail) :- inserido_em([1], 1, 2, [1,2]).	
+	test(inserido_em4) :- inserido_em([2,3], 1, 1, [1,2,3]).	
+	test(inserido_em5) :- inserido_em([1,2], 4, 3, [1,2,4]).
+	test(inserido_em6, fail) :- inserido_em([1], 2, 2, [1,4]).
 
-:- end_tests(removido_em).
+:- end_tests(inserido_em).
          
-removido_em( [X|Y], X, 1, Y ) :- !.
+inserido_em( Y, X, 1, [X|Y] ) :- !.
 
-removido_em( [U|Y], X, I, [U|H] ) :-
+inserido_em( [U|Y], X, I, [U|H] ) :-
 I > 0,
 I0 is I - 1, 
-removido_em( Y, X, I0, H ).
+inserido_em( Y, X, I0, H ).
 
 
 
