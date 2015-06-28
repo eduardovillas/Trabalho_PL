915175683411637c116ea7dcdb3248496e1458c6
lucasmartioli <lucasmartioli12@hotmail.com>
2015-06-28 13:41:42 -0300

    Pronto

--- exercicio_3_5.pl
+++ exercicio_3_5.pl
@@ -1,24 +1,14 @@
 
-%% maximo(+XS, ?M) is semidet
+%% palindromo(+L) is determinist
 %
-% ´e verdadeiro se M ´e o valor m´aximo da lista XS.
+% ´e verdadeiro se L é um palindromo
 
-:- begin_tests(maximo).
+:- begin_tests(palindromo).
 
-	test(maximo1, fail) :- maximo([1,2,3], 2).
-	test(maximo2) :- maximo([1,3,3,5], 5).
-	test(maximo3) :- maximo([1,3,3,5,4,7], 7).
-	test(maximo4) :- maximo([1,3,3,9,5,4,7], 9).
-	test(maximo5, Y == 75) :- maximo([1,3,3,9,75,4,7], Y).
+	test(palindromo1, fail) :- palindromo([1,2,3,1,1]).
+	test(palindromo2) :- palindromo([1,2,1]).	
 
-:- end_tests(maximo).
+:- end_tests(palindromo).
 
-maximo([X],X) :- !.
-maximo([X|Y],M):-
-maximo(Y,M0),
-maior_entre(X,M0,M).
-	
-maior_entre( X, Y, X):- X > Y, !.
-maior_entre( _, Y, Y ).
-
-
+palindromo(L):-
+  reverse(L, L).
