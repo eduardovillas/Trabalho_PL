52a853d61db6b6bf2297225c90bae265597610ee
lucasmartioli <lucasmartioli12@hotmail.com>
2015-06-28 23:04:29 -0300

    Ta quase la mesmo

--- exercicio_3_8.pl
+++ exercicio_3_8.pl
@@ -1,18 +1,18 @@
 
 %% sublista(+L, +I, +J, ?R) is semidet
 %
-% é verdadeiro se S é uma sub lista de L com os elementos das posições de I a J (inclusive).
+% é verdadeiro se R é uma sub lista de L com os elementos das posições de I a J (inclusive).
 
 :- begin_tests(sublista).
 
 	test(sublista1) :- sublista([1,2,3], 2, 2, [2]).			
 	test(sublista2, fail) :- sublista([1], 1, 1, [2]).		
 	test(sublista3) :- sublista([1,2,3,4,5,6], 2, 6, [2,3,4,5,6]).		
-	test(sublista4, fail) :- sublista([1], 1, 2, [1]).		
+	test(sublista4, fail) :- sublista([1], 1, 1, [1,2]).		
 
 :- end_tests(sublista).
 
-sublista( L, 1, 1, R ).
+sublista( [], 1, 1, [] ).
 
 sublista( [U|Y], 1, J, [U|R] ) :-
 J > 1,
@@ -23,6 +23,11 @@ sublista( [U|Y], I, J, R ) :-
 I > 1,
 I0 is I - 1, 
 sublista( Y, I0, J, R ).
+
+sublista( [U|_], 1, J, [U] ).
+
+
+
 
 
 
