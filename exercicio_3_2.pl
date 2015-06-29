bb6eb19bfd22b540943237a83c0e228f0e189aee
eduardovillas <eduardovillasboas@sgsistemas.com.br>
2015-06-29 11:01:19 -0300

    Update exercicio_3_2.pl
    falta criar os testes

--- exercicio_3_2.pl
+++ exercicio_3_2.pl
@@ -1,15 +1,12 @@
 
 %% pares(+L, ?P ) is semidet
 %
-% verdadeiro se P são apenas os elementos pares de L. (na mesma ordem).
+% verdadeiro se P sÃ£o apenas os elementos pares de L. (na mesma ordem).
 
-:- begin_tests(pares).
 
-:- end_tests(pares).
-
-pares([],_) :- !.
+pares([],[]) :- !.
 pares([Y|L], X) :- e_impar(Y), pares(L, X). 
-pares([Y|L], [_|Y]) :- e_par(Y), pares(L, [_|Y]).
+pares([Y|L], [Y|Z]) :- e_par(Y), pares(L,Z).
 
 e_par(X) :- X mod 2 =:= 0.
 e_impar(X) :- X mod 2 =\= 0.
