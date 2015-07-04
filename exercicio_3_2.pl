b25446c851f5b754b41eb7b0552f71aee101e8b6
eduardo villas boas dos santos <eduardo.dabrisa@gmail.com>
2015-07-04 16:33:44 -0300

    exercicio 3.2

--- exercicio_3_2.pl
+++ exercicio_3_2.pl
@@ -5,11 +5,22 @@
 
 :- begin_tests(pares).
 
+    test(pares1, fail) :- pares([1,2,3,4], [2,4,3]).
+    test(pares2) :- pares([1,2,3,4], [2,4]).
+    test(pares3, fail) :- pares([1,2,3,4], [2,4,4]).
+    test(pares4) :- pares([1,2,3,4,4,8,10], [2,4,4,8,10]).
+    test(pares4) :- pares([1,2,3,4,4,8,7,3,5,10], [2,4,4,8,10]).
+    test(pares5) :- pares([1,2,3,4,3,3,2], [2,4,2]).
+    test(pares6, fail) :- pares([2,4,2], [1,2,3,4,3,3,2]).
+    test(pares7) :- pares([2,4,2], [2,4,2]).
+    test(pares7, fail) :- pares([2,4,2], [2,4,2,2]).
+
 :- end_tests(pares).
 
-pares([],_) :- !.
-pares([Y|L], X) :- e_impar(Y), pares(L, X). 
-pares([Y|L], [_|Y]) :- e_par(Y), pares(L, [_|Y]).
+pares([],[]) :- !.
+
+pares([Y|L], X) :- e_impar(Y), !, pares(L, X). 
+pares([Y|L], [Y|L2]) :- e_par(Y), !, pares(L, L2).
 
 e_par(X) :- X mod 2 =:= 0.
 e_impar(X) :- X mod 2 =\= 0.
