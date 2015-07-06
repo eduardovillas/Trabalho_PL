49d76b9e798f8fedb7bdae445d15a9a42251b330
lucasmartioli <lucasmartioli12@hotmail.com>
2015-07-06 00:40:18 -0300

    fim lista 3

--- exercicio_3_16.pl
+++ exercicio_3_16.pl
@@ -1,15 +1,28 @@
+%% num_folhas(?T, ?S) is semidet
+%
+% e verdadeiro se S ´e o n´umero de folhas da ´arvore bin´aria T.
+
+
+:- begin_tests(num_folhas).
+
+	test(num_folhas1, fail) :- num_folhas(nodo(x,nodo(x,nill,nill),nill), 3).
+	test(num_folhas2) :- num_folhas(nodo(x,nodo(x,nill,nill),nill), 1).	
+	test(num_folhas3) :- num_folhas(nodo(x,nodo(x,nill,nill),nodo(x,nill,nill)), 2).	
+	test(num_folhas4) :- num_folhas(nodo(x,nodo(x,nill,nill),nodo(x,nodo(x,nill,nill),nill)), 2).	
+
+:- end_tests(num_folhas).
 
 num_folhas( nill, 0 ).
 num_folhas( nodo(_,nill,nill), 1 ).
 num_folhas( nodo(_,Y,Z), S ) :- num_folhas( Y, N ), num_folhas( Z, J ), N0 is N + J, S is N0.
 
 
-arv_bin(nill).
-arv_bin(nodo(_,Esq,Dir)) :- arv_bin(Esq), arv_bin(Dir).
+:- begin_tests(nodoFolha).
 
-na_arv(X,nodo(X,_,_)).
-na_arv(X,nodo(_,Esq,_)) :- na_arv(X,Esq).
-na_arv(X,nodo(_,_,Dir)) :- na_arv(X,Dir).
+	test(nodoFolha1, fail) :- num_folhas(nodo(x,nodo(x,nill,nill),nill)).	
+	test(nodoFolha2) :- num_folhas(nodo(x,nill,nill)).	
+
+:- end_tests(nodoFolha).
 
 nodo(_,_,_).
 nodoFolha( nodo(_,nill,nill) ).
--- lista_3.pl
+++ lista_3.pl
@@ -366,4 +366,31 @@ aplainada([X|Xs],Zs) :- aplainada(X,Y), aplainada(Xs,Ys), append(Y,Ys,Zs).
 arvore(nill).
 arvore(nodo(_,Esq,Dir)) :- arvore(Esq), arvore(Dir).
 
+%% num_folhas(?T, ?S) is semidet
+%
+% e verdadeiro se S ´e o n´umero de folhas da ´arvore bin´aria T.
+
+
+:- begin_tests(num_folhas).
+
+	test(num_folhas1, fail) :- num_folhas(nodo(x,nodo(x,nill,nill),nill), 3).
+	test(num_folhas2) :- num_folhas(nodo(x,nodo(x,nill,nill),nill), 1).	
+	test(num_folhas3) :- num_folhas(nodo(x,nodo(x,nill,nill),nodo(x,nill,nill)), 2).	
+	test(num_folhas4) :- num_folhas(nodo(x,nodo(x,nill,nill),nodo(x,nodo(x,nill,nill),nill)), 2).	
+
+:- end_tests(num_folhas).
+
+num_folhas( nill, 0 ).
+num_folhas( nodo(_,nill,nill), 1 ).
+num_folhas( nodo(_,Y,Z), S ) :- num_folhas( Y, N ), num_folhas( Z, J ), N0 is N + J, S is N0.
+
+
+:- begin_tests(nodoFolha).
+
+	test(nodoFolha1, fail) :- nodoFolha(nodo(x,nodo(x,nill,nill),nill)).	
+	test(nodoFolha2) :- nodoFolha(nodo(x,nill,nill)).	
+
+:- end_tests(nodoFolha).
+
 nodo(_,_,_).
+nodoFolha( nodo(_,nill,nill) ).
